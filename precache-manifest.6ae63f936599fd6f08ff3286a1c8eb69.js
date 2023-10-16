0cd9585693624aae067c6f6b54e2ed6902b1594e
sps208 <satriops20@gmail.com>
2023-10-16 16:27:59 +0700

    update

---
+++ precache-manifest.6ae63f936599fd6f08ff3286a1c8eb69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38c361509794cf06f17eeed419285cd6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4445283b9e64782bf399",
+    "url": "./static/css/main.11b99a3f.chunk.css"
+  },
+  {
+    "revision": "7fef6345df64c15ccf0a",
+    "url": "./static/js/2.696c356e.chunk.js"
+  },
+  {
+    "revision": "65089b70899301b7933aa2160bb4101d",
+    "url": "./static/js/2.696c356e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4445283b9e64782bf399",
+    "url": "./static/js/main.12045a99.chunk.js"
+  },
+  {
+    "revision": "7d40e84f1f69f03e2f1d",
+    "url": "./static/js/runtime-main.b10797a6.js"
+  },
+  {
+    "revision": "5ff680ea100cc6621b43fc99d6235ecb",
+    "url": "./static/media/FotoProfile.5ff680ea.jpg"
+  }
+]);
