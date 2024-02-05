06f858ee75e1ecaa91f222582227369b7206e1a7
SatrioPrawiro <49888578+sps208@users.noreply.github.com>
2024-02-05 15:26:05 +0700

    create

---
+++ precache-manifest.dd6d09c76d11e9b13af2f56e2ad240e0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffcd797907e8c068d1aa379fba49049a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2027f5a0cb5c6bdfc65c",
+    "url": "./static/css/main.517a74db.chunk.css"
+  },
+  {
+    "revision": "f4385e8852d2969ad6ce",
+    "url": "./static/js/2.84d9ca51.chunk.js"
+  },
+  {
+    "revision": "65089b70899301b7933aa2160bb4101d",
+    "url": "./static/js/2.84d9ca51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2027f5a0cb5c6bdfc65c",
+    "url": "./static/js/main.f7e70ec2.chunk.js"
+  },
+  {
+    "revision": "7d40e84f1f69f03e2f1d",
+    "url": "./static/js/runtime-main.b10797a6.js"
+  },
+  {
+    "revision": "b3d18cd5e32f6f1ff1347fa05008eb7e",
+    "url": "./static/media/CV_SatrioPrawiro_Pajakku_compressed.b3d18cd5.pdf"
+  },
+  {
+    "revision": "5ff680ea100cc6621b43fc99d6235ecb",
+    "url": "./static/media/FotoProfile.5ff680ea.jpg"
+  },
+  {
+    "revision": "34377beedcf1ab7a6ee0565899f77a99",
+    "url": "./static/media/ezgif-5-7453034699.34377bee.gif"
+  }
+]);
